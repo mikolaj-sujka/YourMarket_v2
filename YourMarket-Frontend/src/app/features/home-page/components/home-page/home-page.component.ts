8650a2657c6f34038084c8638cd1c49330da3522
Mikołaj Sujka <mikolaj.sujka@qodeca.com>
2024-05-28 18:51:46 +0200

    Working on nav

--- YourMarket-Frontend/src/app/features/home-page/components/home-page/home-page.component.ts
+++ YourMarket-Frontend/src/app/features/home-page/components/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../../../core/components/nav/nav.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
